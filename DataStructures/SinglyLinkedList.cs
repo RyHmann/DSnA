1ec733ce65ed4b69e8b8d002cc007cb3ff724008
Ryan Heitmann <ryan.heitmann@gmail.com>
2022-07-18 08:31:06 +1200

    Add project files.

---
+++ DataStructures/SinglyLinkedList.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class SinglyLinkedList
+    {
+        public SNode? Head { get; set; } = null;
+
+        public SinglyLinkedList()
+        {
+
+        }
+
+        public SinglyLinkedList(SNode newNode)
+        {
+            Head = newNode; 
+        }
+
+        public void AddNode(int value)
+        {
+            if (Head == null)
+            {
+                Head = new SNode(value);
+            }
+
+            else if (Head.Next is null)
+            {
+                Head.Next = new SNode(value);
+            }
+            else
+            {
+                var tail = Head.Next;
+
+                while (tail.Next != null)
+                {
+                    tail = tail.Next;
+                }
+                tail.Next = new SNode(value);
+            }
+        }
+
+        public void DeleteNode(int index)
+        {
+            int counter = 0;
+
+            // Iterate through each .Next
+            // Increment counter
+            // Stop when counter == index
+            var previousNode = Head;
+            if (Head.Next is not null)
+            {
+                var currentNode = previousNode.Next;
+                counter++;
+
+                while (currentNode.Next is not null)
+                {
+                    previousNode = currentNode;
+                    currentNode = currentNode.Next;
+                    counter++;
+                    if (counter == index)
+                    {
+                        previousNode.Next = currentNode.Next;
+                    }
+                }
+            }
+        }
+
+        public void InsertNode(int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class SNode
+    {
+        public int Value { get; set; }
+        public SNode? Next { get; set; }
+
+        public SNode(int value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+}
---
+++ UI/Program.cs
@@ -0,0 +1,51 @@
+﻿using DataStructures;
+
+var testList = new SinglyLinkedList();
+var numArray = new int[] { 7, 9, 3, 1, 5, 9, 10, 153, 989, 13, 498, 16 };
+
+testList.Head = new SNode(5);
+
+
+testList.AddNode(7);
+testList.AddNode(21);
+testList.AddNode(2);
+testList.AddNode(965);
+testList.AddNode(1);
+testList.AddNode(400);
+
+PrintAllNodes(testList);
+
+//Delete '2' at index 3
+testList.DeleteNode(3);
+PrintAllNodes(testList);
+
+testList.DeleteNode(3);
+PrintAllNodes(testList);
+
+
+
+static void PrintLastNode(SinglyLinkedList list)
+{
+    var currentNode = list.Head;
+
+    while (currentNode.Next != null)
+    {
+        currentNode = currentNode.Next;
+    }
+
+    Console.WriteLine(currentNode.Value);
+}
+
+static void PrintAllNodes(SinglyLinkedList list)
+{
+    Console.WriteLine(String.Empty);
+    var currentNode = list.Head;
+    Console.Write($"{currentNode.Value}" + ',');
+    while (currentNode.Next != null)
+    {
+        currentNode = currentNode.Next;
+        Console.Write($"{currentNode.Value}" + ',');
+    }
+}
+
+
